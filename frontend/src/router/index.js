c91cb9580845e304905f8e50ff4ea6ca618bf849
Nqtural <robin.stalstad@proton.me>
2026-01-30 14:13:18 +0100

    chore(frontend): comply with linter suggestions

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
 import Login from "../components/Login.vue";
-import Register from "../components/Register.vue"
+import Register from "../components/Register.vue";
 
 const routes = [
 	{ path: "/home", component: Home },
 	{ path: "/login", component: Login },
-	{ path: "/register", component: Register }
+	{ path: "/register", component: Register },
 ];
 
 export default createRouter({
