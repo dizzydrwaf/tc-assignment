5ccfb7cc2e4baa12e2477e94efae0e8e59eebe6b
Nqtural <robin.stalstad@proton.me>
2026-02-03 23:57:39 +0100

    fix(backend): revert `async` keyword removal
    Making function `health` synchronous removed the ability for axum
    routing to automatically implement trait `Handler<_, _>`. Add Clippy
    ignore and revert change.

--- backend/src/routes/health.rs
+++ backend/src/routes/health.rs
@@ -6,6 +6,8 @@ pub struct Health {
     status: &'static str,
 }
 
-pub const fn health() -> Json<Health> {
+// Needs async for axum to implement trait `Handler<_, _>`
+#[allow(clippy::unused_async)]
+pub async fn health() -> Json<Health> {
     Json(Health { status: "ok" })
 }
